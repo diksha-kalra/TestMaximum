9d4b32b01c201f10347c1659b668ea13685786b6
diksha-kalra <kalradiksha11@gmail.com>
2020-10-09 00:59:41 +0530

    Added: generic print method

--- main/java/com/cg/findmaximum/Maximum.java
+++ main/java/com/cg/findmaximum/Maximum.java
@@ -9,6 +9,12 @@ public class Maximum<T extends Comparable<T>> {
 	public static <T extends Comparable<T>> T maximum(T ... values) {
 		Arrays.sort(values);
 		T max=values[values.length-1];
+		printMax(max);
 		return max;
+	}
+	
+	public static <T> void printMax(T max) {
+		System.out.printf("Maximum :" +max);
+		System.out.println("\n");
 	}
 }
